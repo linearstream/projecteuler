e30235040e88f1499a1d91b37a034cb5006e682f
linearstream <664134+linearstream@users.noreply.github.com>
2020-01-05 12:47:42 -0500

    finished c setup

---
+++ 1/c/multiples_3_5.c
@@ -0,0 +1,18 @@
+/* File: multiples_3_5.c
+ * Author: Joshua Thies
+ * Description: Sum the uniq multiples of 3 and 5 less than 100
+ */
+
+#include <stdio.h>
+#include "multiples_3_5.h"
+
+// main:
+//   Accepts CLI arguments and runs the program
+int main(int argc, char *argv[]) {
+	printf("%s\n", greeting());
+	return 0;
+}
+
+char* greeting() {
+	return "hello world";
+}
